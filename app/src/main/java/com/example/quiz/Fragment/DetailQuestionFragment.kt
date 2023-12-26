688bd7ed61b78b2203dfc5dee4d027462c20bb1a
venlaken123 <datnm212@gmail.com>
2023-12-26 13:06:51 +0700

    update all 3

--- app/src/main/java/com/example/quiz/Fragment/DetailQuestionFragment.kt
+++ app/src/main/java/com/example/quiz/Fragment/DetailQuestionFragment.kt
@@ -1,7 +1,9 @@
 package com.example.quiz.Fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -40,11 +42,14 @@ class DetailQuestionFragment : Fragment() {
     private lateinit var finishButton : Button
     private lateinit var nextButton : Button
     private lateinit var questionTv : TextView
-    private lateinit var answerTv : TextView
-    private lateinit var questionNumberTv : TextView
+    private lateinit var correctTv : TextView
+    private lateinit var wrongTv : TextView
     private lateinit var timerCount : TextView
 
 
+    private var timer: CountDownTimer? = null
+    private var correctAnswers = 0
+    private var wrongAnswers = 0
 
     override fun onCreateView(
         inflater : LayoutInflater , container : ViewGroup? ,
@@ -65,6 +70,8 @@ class DetailQuestionFragment : Fragment() {
         finishButton = view.findViewById(R.id.btnFinish)
         nextButton = view.findViewById(R.id.btnNext)
         questionTv = view.findViewById(R.id.tvQuizDetail)
+        correctTv = view.findViewById(R.id.textViewCorrect)
+        wrongTv = view.findViewById(R.id.textViewWrong)
         timerCount = view.findViewById(R.id.tvCountTimer)
 
         quizDetailViewModel.quizzes.observe(viewLifecycleOwner) {  quizzes ->
@@ -76,78 +83,123 @@ class DetailQuestionFragment : Fragment() {
                 Log.d("OptionTest", "${quizDetailViewModel.getCurrentQuiz()?.optionA}")
             }
         }
-
         // Load the next question on button click
         nextButton.setOnClickListener {
-            val currentIndex = quizDetailViewModel.currentQuizIndex.value ?: 0
-            if (currentIndex < (quizDetailViewModel.quizzes.value?.size ?: 0) - 1) {
-                quizDetailViewModel.setCurrentQuizIndex(currentIndex + 1)
-                displayQuizData(quizDetailViewModel.getCurrentQuiz())
-            } else {
-                // Handle when all questions are finished
-                navController.navigate(R.id.action_detailQuestionFragment_to_resultFragment)
-            }
+            moveToNextQuestion()
         }
-
         // Fetch quizzes
         quizDetailViewModel.getQuizzes()
     }
+    private fun moveToNextQuestion(){
+        val currentIndex = quizDetailViewModel.currentQuizIndex.value ?: 0
+        if (currentIndex < (quizDetailViewModel.quizzes.value?.size ?: 0) - 1) {
+            quizDetailViewModel.setCurrentQuizIndex(currentIndex + 1)
+            resetUI()
+            displayQuizData(quizDetailViewModel.getCurrentQuiz())
+        } else {
+            // Handle when all questions are finished
+            navController.navigate(R.id.action_detailQuestionFragment_to_resultFragment)
+        }
+    }
+
+    private fun resetUI() {
+        optionAButton.setBackgroundColor(Color.WHITE)
+        optionBButton.setBackgroundColor(Color.WHITE)
+        optionCButton.setBackgroundColor(Color.WHITE)
+        optionDButton.setBackgroundColor(Color.WHITE)
+        optionAButton.isEnabled = true
+        optionBButton.isEnabled = true
+        optionCButton.isEnabled = true
+        optionDButton.isEnabled = true
+        timer?.cancel()
+        timerCount.text = ""
+    }
 
     private fun displayQuizData(quizDetail: QuizDetailModel?) {
-        quizDetail?.let { it ->
+
+        timer?.cancel()
+
+        quizDetail?.let { detail ->
             // Display the question
-            questionTv.text = it.question
+            questionTv.text = detail.question
 
             // Display the options
-            optionAButton.text = it.optionA
-            Log.d("OptionTest", "${it.optionA}")
-            optionBButton.text = it.optionB
-            optionCButton.text = it.optionC
-            optionDButton.text = it.optionD
+            optionAButton.text = detail.optionA
+            optionBButton.text = detail.optionB
+            optionCButton.text = detail.optionC
+            optionDButton.text = detail.optionD
+
+            optionAButton.setOnClickListener {
+                checkAnswer(detail.optionA, detail.answer)
+                disableButtons()
+            }
+            optionBButton.setOnClickListener {
+                checkAnswer(detail.optionB, detail.answer)
+                disableButtons()
+            }
+            optionCButton.setOnClickListener {
+                checkAnswer(detail.optionC, detail.answer)
+                disableButtons()
+            }
+            optionDButton.setOnClickListener {
+                checkAnswer(detail.optionD, detail.answer)
+                disableButtons()
+            }
 
             // Set up timer
-            val timer = object : CountDownTimer(it.timer * 1000, 1000) {
+            timer = object : CountDownTimer(detail.timer * 1000, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
                     timerCount.text = (millisUntilFinished / 1000).toString()
                 }
 
                 override fun onFinish() {
                     timerCount.text = "Time's up!"
+                    disableButtons()
+                    //Bổ sung logic khi thời gian hết
                 }
-            }
-            timer.start()
-
-            // Handle user option selection
+            }.start()
         }
     }
-//    private fun checkAnswer(selectedAnswer: String) {
-//        val currentQuiz = quizDetailViewModel.getCurrentQuiz()
-//        currentQuiz?.let { quiz ->
-//            val correctAnswer = quiz.answer
-//
-//            // Check if the selected answer is correct
-//            val isCorrect = selectedAnswer == correctAnswer
-//
-//            // Perform actions based on the correctness of the answer
-//            if (isCorrect) {
-//                // If the answer is correct, you can display a message or perform any action
-//                // For example, display a toast indicating the correct answer
-//                Toast.makeText(requireContext(), "Correct Answer!", Toast.LENGTH_SHORT).show()
-//            } else {
-//                // If the answer is incorrect, you can display a message or perform any action
-//                // For example, display a toast indicating the wrong answer and show the correct answer
-//                val correctAnswerMessage = "Wrong Answer! Correct Answer is: $correctAnswer"
-//                Toast.makeText(requireContext(), correctAnswerMessage, Toast.LENGTH_SHORT).show()
-//            }
-//
-//            // Proceed to the next question if available or navigate to the result fragment
-//            val currentIndex = quizDetailViewModel.currentQuizIndex.value ?: 0
-//            if (currentIndex < (quizDetailViewModel.quizzes.value?.size ?: 0) - 1) {
-//                quizDetailViewModel.setCurrentQuizIndex(currentIndex + 1)
-//                displayQuizData(quizDetailViewModel.getCurrentQuiz())
-//            } else {
-//                navController.navigate(R.id.action_detailQuestionFragment_to_resultFragment)
-//            }
-//        }
-//    }
+
+    private fun disableButtons() {
+        // Vô hiệu hóa tất cả các nút tương ứng
+        optionAButton.isEnabled = false
+        optionBButton.isEnabled = false
+        optionCButton.isEnabled = false
+        optionDButton.isEnabled = false
+    }
+
+    private fun checkAnswer(selectedAnswer: String, correctAnswer: String) {
+        if (selectedAnswer == correctAnswer) {
+            correctAnswers++
+            correctTv.text = correctAnswers.toString()
+            // Handle correct answer UI
+            when (selectedAnswer) {
+                optionAButton.text.toString() -> setButtonBackground(optionAButton, true)
+                optionBButton.text.toString() -> setButtonBackground(optionBButton, true)
+                optionCButton.text.toString() -> setButtonBackground(optionCButton, true)
+                optionDButton.text.toString() -> setButtonBackground(optionDButton, true)
+            }
+        } else {
+            wrongAnswers++
+            wrongTv.text = wrongAnswers.toString()
+            when (selectedAnswer) {
+                optionAButton.text.toString() -> setButtonBackground(optionAButton, false)
+                optionBButton.text.toString() -> setButtonBackground(optionBButton, false)
+                optionCButton.text.toString() -> setButtonBackground(optionCButton, false)
+                optionDButton.text.toString() -> setButtonBackground(optionDButton, false)
+            }
+            // Update UI for wrong answer
+            // For example, change the background color of the selected button to red
+        }
+    }
+
+    private fun setButtonBackground(button : Button , isCorrect : Boolean) {
+        if (isCorrect) {
+            button.setBackgroundColor(Color.GREEN)
+        }
+        else {
+            button.setBackgroundColor(Color.RED)
+        }
+    }
 }
